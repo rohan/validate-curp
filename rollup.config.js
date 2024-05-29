f318e21e4e725b0f9f1b99e84c70850779fa121f
Rohan Ramchand <rohan@neon.xyz>
2024-05-29 11:57:57 -0400

    Add type definitions to output

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,33 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'umd',
-    name: 'validateCurp',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'validateCurp',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      json(),
+      babel({
+        babelHelpers: 'bundled',
+        presets: ['@babel/env'],
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    commonjs(),
-    json(),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/env'],
-    }),
-    terser(),
-  ],
-};
+  {
+    input: 'src/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
