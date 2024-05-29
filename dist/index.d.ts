89b76728fe91f183d3daa3a6efdf459af35f2e11
Rohan Ramchand <rohan@neon.xyz>
2024-05-29 12:01:21 -0400

    update output

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+declare function validateCurp<T extends string>(curp: T): {
+  isValid: boolean,
+  curp?: T,
+  errors?: ('INVALID_FORMAT' | 'INVALID_DATE' | 'INVALID_STATE' | 'INVALID_VERIFICATION_DIGIT' | 'FORBIDDEN_WORD')[]
+};
+
+export { validateCurp as default };
